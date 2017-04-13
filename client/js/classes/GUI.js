d5286cae17aca7ef080ec4244e9369443d6676cf
Davis Yarbrough <dyarbrough@nosco.com>
2017-04-12 20:09:55 -0500

    GUI documented

--- client/js/classes/GUI.js
+++ client/js/classes/GUI.js
@@ -1,9 +1,29 @@
+'use strict'
+
+/**
+ * Manages the game's dat.GUI
+ * @namespace GUI
+ */
 var GUI = function(window, undefined) {
+
+    /*------------------------------------*
+     :: Class Variables
+     *------------------------------------*/
 
     var settings
     var gui
     var guiClicked
 
+    /*------------------------------------*
+     :: Public Methods
+     *------------------------------------*/
+
+    /**
+     * Initializes the module. Must be called
+     * before anything else
+     * @memberOf GUI
+     * @access public
+     */
     function init() {
 
         settings = {
@@ -45,6 +65,106 @@ var GUI = function(window, undefined) {
 
     }
 
+    /**
+     * Set the specified saved color to the block color
+     * @memberOf GUI
+     * @access public
+     * @param {Number} cNum The saved color number
+     */
+    function setSavedColor(cNum) {
+        settings.colors.saved[cNum] = settings.colors.blockColor
+    }
+
+    /**
+     * Set the block color to the specified saved color
+     * @memberOf GUI
+     * @access public
+     * @param {Number} cNum The saved color number
+     */
+    function loadSavedColor(cNum) {
+        settings.colors.blockColor = settings.colors.saved[cNum]
+    }
+
+    /**
+     * Given an intersect, extract the color of the intersected
+     * block (if it was a block that was intersected) and assign it
+     * to the blockColor + update the Game scenes ghost mesh
+     * @memberOf GUI
+     * @access public
+     * @param {THREE.Intersect} intersect The intersect
+     */
+    function setPickColor(intersect) {
+
+        var iObj = intersect.object
+        var objName = iObj.name
+
+        var pickColor
+
+        if (objName !== 'plane') {
+
+            if (objName === 'BufferMesh') {
+
+                var bufColArr = iObj.geometry.attributes.color.array
+                var idx = intersect.index * 3
+
+                pickColor = new THREE.Color(bufColArr[idx], bufColArr[idx + 1], bufColArr[idx + 2])
+
+            } else { // voxel
+
+                pickColor = intersect.face.color
+
+            }
+
+            var hColor = pickColor.getHex()
+
+            settings.colors.blockColor = hColor
+            UserState.setDefaultState()
+            GameScene.setGhostMeshColor(hColor ^ 0x4C000000)
+
+        }
+
+    }
+
+    /**
+     * Get the current block color
+     * @memberOf GUI
+     * @access public
+     * @return {Number} The block color
+     */
+    function getBlockColor() {
+        return settings.colors.blockColor
+    }
+
+    /**
+     * Return whether or not the GUI was click
+     * before the mouse click event was received
+     * @memberOf GUI
+     * @access public
+     * @return {Number} The block color
+     */
+    function wasClicked() {
+        return guiClicked
+    }
+
+    /**
+     * Set clicked to true or false
+     * @memberOf GUI
+     * @access public
+     * @param clicked What to set it to
+     */
+    function setClicked(clicked) {
+        guiClicked = clicked
+    }
+
+    /*------------------------------------*
+     :: Private Methods
+     *------------------------------------*/
+
+    /**
+     * Add the necessary elements to the gui
+     * @memberOf GUI
+     * @access private
+     */
     function addGUIEls() {
 
         var colors = gui.addFolder('Colors')
@@ -79,72 +199,39 @@ var GUI = function(window, undefined) {
 
     }
 
-    function setSavedColor(cNum) {
-        settings.colors.saved[cNum] = settings.colors.blockColor
-    }
-
-    function loadSavedColor(cNum) {
-        settings.colors.blockColor = settings.colors.saved[cNum]
-    }
-
-    function setPickColor(intersect) {
-
-        var iObj = intersect.object
-        var objName = iObj.name
-
-        var pickColor
-
-        if (objName !== 'plane') {
-
-            if (objName === 'BufferMesh') {
-
-                var bufColArr = iObj.geometry.attributes.color.array
-                var idx = intersect.index * 3
-
-                pickColor = new THREE.Color(bufColArr[idx], bufColArr[idx + 1], bufColArr[idx + 2])
-
-            } else { // voxel
-
-                pickColor = intersect.face.color
-
-            }
-
-            var hColor = pickColor.getHex()
-
-            settings.colors.blockColor = hColor
-            UserState.setDefaultState()
-            GameScene.setGhostMeshColor(hColor ^ 0x4C000000)
-
-        }
-
-    }
-
+    /**
+     * If the pick color button is clicked,
+     * set the users state to pickcolor
+     * @memberOf GUI
+     * @access private
+     */
     function pickColor() {
         if (UserState.modeIsEdit())
             UserState.setPickState()
     }
 
+    /**
+     * Set the block color to a random color
+     * @memberOf GUI
+     * @access private
+     */
     function setRandomBlockColor() {
         var randColor = randomHexColor()
         GameScene.setGhostMeshColor(randColor)
         settings.colors.blockColor = randColor
     }
 
+    /**
+     * Get a random hex color
+     * @memberOf GUI
+     * @access private
+     * @return {Number} The random hex color
+     */
     function randomHexColor() {
         return 0xffffff * Math.random()
     }
 
-    function getBlockColor() {
-        return settings.colors.blockColor
-    }
-
-    function wasClicked() {
-        return guiClicked
-    }
-
-    function setClicked(clicked) {
-        guiClicked = clicked
-    }
+    /*********** expose public methods *************/
 
     return {
         init: init,
