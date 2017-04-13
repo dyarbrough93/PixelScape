7c9f63ff6967208ea5760437e10126428223d1d1
Davis Yarbrough <dyarbrough@nosco.com>
2017-04-13 13:29:58 -0500

    grunt obfuscation

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,42 +5,47 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         uglify: {
-            client: {
-                // Grunt will search for "**/*.js" under "lib/" when the "uglify" task
-                // runs and build the appropriate src-dest file mappings then, so you
-                // don't need to update the Gruntfile when files are added or removed.
+            lib: {
+                files: [{
+                    expand: true, // Enable dynamic expansion.
+                    cwd: 'client/js/lib/', // Src matches are relative to this path.
+                    src: ['*.js'], // Actual pattern(s) to match.
+                    dest: 'build/js/lib/', // Destination path prefix.
+                    ext: '.min.js', // Dest filepaths will have this extension.
+                    extDot: 'last' // Extensions in filenames begin after the last dot
+                }]
+            }
+        },
+        obfuscator: {
+            classes: {
                 files: [{
                     expand: true, // Enable dynamic expansion.
                     cwd: 'client/js/', // Src matches are relative to this path.
-                    src: ['**/*.js'], // Actual pattern(s) to match.
+                    src: ['*.js', 'classes/*.js'], // Actual pattern(s) to match.
                     dest: 'build/js/', // Destination path prefix.
-                    ext: '.min.js', // Dest filepaths will have this extension.
+                    ext: '.obs.js', // Dest filepaths will have this extension.
                     extDot: 'last' // Extensions in filenames begin after the last dot
                 }]
             }
         },
         copy: {
             css: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: 'client/css/',
-                        src: ['*'],
-                        dest: 'build/css',
-                        filter: 'isFile'
-                    }
-                ]
+                files: [{
+                    expand: true,
+                    cwd: 'client/css/',
+                    src: ['*'],
+                    dest: 'build/css',
+                    filter: 'isFile'
+                }]
             },
             img: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: 'client/img/',
-                        src: ['*'],
-                        dest: 'build/img',
-                        filter: 'isFile'
-                    }
-                ]
+                files: [{
+                    expand: true,
+                    cwd: 'client/img/',
+                    src: ['*'],
+                    dest: 'build/img',
+                    filter: 'isFile'
+                }]
             }
         }
     })
@@ -48,8 +53,9 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-newer')
     grunt.loadNpmTasks('grunt-contrib-copy')
     grunt.loadNpmTasks('grunt-contrib-uglify')
+    grunt.loadNpmTasks('grunt-contrib-obfuscator')
 
     // Default task(s).
-    grunt.registerTask('default', ['newer:uglify', 'newer:copy'])
+    grunt.registerTask('default', ['newer:obfuscator', 'newer:uglify', 'newer:copy'])
 
 };
