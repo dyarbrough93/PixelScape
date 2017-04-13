3b6da534127fd1889321a75efa92d08689e767f6
Davis Yarbrough <dyarbrough@nosco.com>
2017-04-12 21:24:59 -0500

    SocketHandler documented

--- client/js/classes/SocketHandler.js
+++ client/js/classes/SocketHandler.js
@@ -1,7 +1,27 @@
+'use strict'
+
+/**
+ * Manages socket events
+ * @namespace SocketHandler
+ */
 var SocketHandler = function(window, undefined) {
+
+    /*------------------------------------*
+     :: Class Variables
+     *------------------------------------*/
 
     var socket
 
+    /*------------------------------------*
+     :: Public Methods
+     *------------------------------------*/
+
+    /**
+     * Initializes the module. Must be called
+     * before anything else
+     * @memberOf SocketHandler
+     * @access public
+     */
     function init() {
 
         socket = io.connect()
@@ -10,6 +30,120 @@ var SocketHandler = function(window, undefined) {
 
     }
 
+    /**
+     * Send a "block removed" socket emit
+     * with the given grid position
+     * @memberOf SocketHandler
+     * @access public
+     * @param  {VoxelUtils.GridVector3} gPos The grid
+     * position of the voxel to remove
+     * @param  {Function} cb Callback to call with
+     * a boolean indicating success
+     */
+    function emitBlockRemoved(gPos, cb) {
+
+        socket.emit('block removed', {
+            x: gPos.x,
+            y: gPos.y,
+            z: gPos.z
+        }, function(response) {
+
+            if (response === 'success')
+                return cb(true)
+            else return cb(false)
+
+        })
+
+    }
+
+    /**
+     * Send a "block added" socket emit
+     * with the given grid position and color
+     * @memberOf SocketHandler
+     * @access public
+     * @param  {VoxelUtils.GridVector3} gPos The grid
+     * position of the voxel to add
+     * @param {Number} hColor Hex color of the voxel
+     * we are adding
+     * @param  {Function} cb Callback to call with
+     * a boolean indicating success
+     */
+    function emitBlockAdded(gPos, hColor, cb) {
+
+        socket.emit('block added', {
+            color: hColor,
+            position: {
+                x: gPos.x,
+                y: gPos.y,
+                z: gPos.z
+            }
+        }, function(response) {
+
+            if (response === 'success')
+                return cb(true)
+            else if (response === 'max') {
+                alert('maximum voxel limit reached.')
+                return cb(false)
+            }
+
+        })
+
+    }
+
+    /**
+     * Retrieve the world data from the server
+     * @param  {Function} cb Callback to call with
+     * the received data as the only parameter
+     */
+    function retrieveData(cb) {
+
+        socket.emit('start chunking')
+
+        var numChunks
+        var numChunksLoaded
+        var chunkData
+
+        // we get the number of chunks we are
+        // about to receive
+        socket.on('chunking size', function(size) {
+            console.log('receiving data from server')
+            numChunks = size
+        })
+
+        chunkData = '{'
+        numChunksLoaded = 0
+
+        // we receive a chunk
+        socket.on('chunk', function(chunk) {
+
+            numChunksLoaded++
+
+            if (numChunks > 0) {
+                var percent = ((numChunksLoaded / numChunks) * 100).toFixed(0)
+                console.log(percent + '% chunks loaded')
+            }
+
+            chunkData += chunk
+        })
+
+        // we have received all chunks
+        socket.on('chunk done', function() {
+            chunkData += '}'
+            chunkData = JSON.parse(chunkData)
+            console.log('done retrieving data')
+            cb(chunkData)
+        })
+    }
+
+    /*------------------------------------*
+     :: Private Methods
+     *------------------------------------*/
+
+    /**
+     * Initialize socket.on events
+     * @memberOf SocketHandler
+     * @access public
+     */
     function initSocketOns() {
 
         socket.on('block added', function(block) {
@@ -51,87 +185,7 @@ var SocketHandler = function(window, undefined) {
 
     }
 
-    function emitBlockRemoved(gPos, cb) {
-
-        socket.emit('block removed', {
-            x: gPos.x,
-            y: gPos.y,
-            z: gPos.z
-        }, function(response) {
-
-            if (response === 'success')
-                return cb(true)
-            else return cb(false)
-
-        })
-
-    }
-
-    function emitBlockAdded(gPos, hColor, cb) {
-
-        socket.emit('block added', {
-            color: hColor,
-            position: {
-                x: gPos.x,
-                y: gPos.y,
-                z: gPos.z
-            }
-        }, function(response) {
-
-            if (response === 'success')
-                return cb(true)
-            else if (response === 'max') {
-                alert('maximum voxel limit reached.')
-                return cb(false)
-            }
-
-        })
-
-    }
-
-    /**************************************\
-    | Receive Chunked Data                 |
-    \**************************************/
-
-    function retrieveData(cb) {
-
-        socket.emit('start chunking')
-
-        var numChunks
-        var numChunksLoaded
-        var chunkData
-
-        // we get the number of chunks we are
-        // about to receive
-        socket.on('chunking size', function(size) {
-            console.log('receiving data from server')
-            numChunks = size
-        })
-
-        chunkData = '{'
-        numChunksLoaded = 0
-
-        // we receive a chunk
-        socket.on('chunk', function(chunk) {
-
-            numChunksLoaded++
-
-            if (numChunks > 0) {
-                var percent = ((numChunksLoaded / numChunks) * 100).toFixed(0)
-                console.log(percent + '% chunks loaded')
-            }
-
-            chunkData += chunk
-        })
-
-        // we have received all chunks
-        socket.on('chunk done', function() {
-            chunkData += '}'
-            chunkData = JSON.parse(chunkData)
-            console.log('done retrieving data')
-            cb(chunkData)
-        })
-    }
+    /*********** expose public methods *************/
 
     return {
         init: init,
