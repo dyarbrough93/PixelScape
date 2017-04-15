8a38bea9492edfe398ecaed0c5ccf8beebc1643a
Davis Yarbrough <dyarbrough@nosco.com>
2017-04-14 20:25:51 -0500

    add logout to GUI, new npm scripts for dev

--- server/passport/init.js
+++ server/passport/init.js
@@ -7,13 +7,13 @@ module.exports = function(passport){
 	// Passport needs to be able to serialize and deserialize users to support persistent login sessions
     passport.serializeUser(function(user, done) {
         console.log('serializing user: ')
-        console.log(user)
+        console.log(user.username)
         done(null, user._id)
     })
 
     passport.deserializeUser(function(id, done) {
         User.findById(id, function(err, user) {
-            console.log('deserializing user:',user)
+            console.log('deserializing user:', user.username)
             done(err, user)
         })
     })
--- server/routes.js
+++ server/routes.js
@@ -3,19 +3,24 @@ const router = express.Router()
 
 var isAuthenticated = function(req, res, next) {
 
-    if (process.env.NODE_ENV === 'dev') return next()
+    if (process.env.USE_LOGIN === 'y') {
 
-    if (req.isAuthenticated())
-        return next()
-    res.redirect('/login')
+        if (req.isAuthenticated())
+            return next()
+        res.redirect('/login')
+
+    } else return next()
+
 }
+
+const dev = process.env.NODE_ENV === 'dev' ? 'dev/' : ''
 
 module.exports = function(passport) {
 
     /* GET login page. */
     router.get('/login', function(req, res) {
         // Display the Login page with any flash message, if any
-        res.render('login', {
+        res.render(dev + 'login', {
             layout: false
         })
     })
@@ -43,15 +48,9 @@ module.exports = function(passport) {
     /* GET Home Page */
     router.get('/', isAuthenticated, function(req, res) {
 
-        if (process.env.NODE_ENV === 'dev') {
-            res.render('dev/devGame', {
-                user: req.user
-            })
-        } else {
-            res.render('game', {
-                user: req.user
-            })
-        }
+        res.render(dev + 'game', {
+            user: req.user
+        })
     })
 
     return router
