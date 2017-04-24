42e6a8ccdc8dd33fe959706a6b0069af835302f7
Davis Yarbrough <dyarbrough@nosco.com>
2017-04-23 21:37:50 -0500

    validate password length and email domain on signup

--- server/passport/signup.js
+++ server/passport/signup.js
@@ -9,7 +9,12 @@ module.exports = function(passport) {
         },
         function(req, username, password, done) {
 
-            console.log('signup')
+			var res = /@[\w-\.]+/.exec(req.body.email)
+            if (!res || domains.indexOf(res[0].substring(1)) === -1) return done(null, false, { message: 'Please use a real email address.'})
+
+            if (password.length < 8) return done(null, false, { message: 'Password must be at least 8 characters.' })
+
+            if (password.length)
 
             findOrCreateUser = function() {
                 // find a user in Mongo with provided username
@@ -61,3 +66,48 @@ module.exports = function(passport) {
     }
 
 }
+
+var domains = [
+  /* Default domains included */
+  "aol.com", "att.net", "comcast.net", "facebook.com", "gmail.com", "gmx.com", "googlemail.com",
+  "google.com", "hotmail.com", "hotmail.co.uk", "mac.com", "me.com", "mail.com", "msn.com",
+  "live.com", "sbcglobal.net", "verizon.net", "yahoo.com", "yahoo.co.uk",
+
+  /* Other global domains */
+  "email.com", "games.com" /* AOL */, "gmx.net", "hush.com", "hushmail.com", "icloud.com", "inbox.com",
+  "lavabit.com", "love.com" /* AOL */, "outlook.com", "pobox.com", "rocketmail.com" /* Yahoo */,
+  "safe-mail.net", "wow.com" /* AOL */, "ygm.com" /* AOL */, "ymail.com" /* Yahoo */, "zoho.com", "fastmail.fm",
+  "yandex.com","iname.com",
+
+  /* United States ISP domains */
+  "bellsouth.net", "charter.net", "comcast.net", "cox.net", "earthlink.net", "juno.com",
+
+  /* British ISP domains */
+  "btinternet.com", "virginmedia.com", "blueyonder.co.uk", "freeserve.co.uk", "live.co.uk",
+  "ntlworld.com", "o2.co.uk", "orange.net", "sky.com", "talktalk.co.uk", "tiscali.co.uk",
+  "virgin.net", "wanadoo.co.uk", "bt.com",
+
+  /* Domains used in Asia */
+  "sina.com", "qq.com", "naver.com", "hanmail.net", "daum.net", "nate.com", "yahoo.co.jp", "yahoo.co.kr", "yahoo.co.id", "yahoo.co.in", "yahoo.com.sg", "yahoo.com.ph",
+
+  /* French ISP domains */
+  "hotmail.fr", "live.fr", "laposte.net", "yahoo.fr", "wanadoo.fr", "orange.fr", "gmx.fr", "sfr.fr", "neuf.fr", "free.fr",
+
+  /* German ISP domains */
+  "gmx.de", "hotmail.de", "live.de", "online.de", "t-online.de" /* T-Mobile */, "web.de", "yahoo.de",
+
+  /* Russian ISP domains */
+  "mail.ru", "rambler.ru", "yandex.ru", "ya.ru", "list.ru",
+
+  /* Belgian ISP domains */
+  "hotmail.be", "live.be", "skynet.be", "voo.be", "tvcablenet.be", "telenet.be",
+
+  /* Argentinian ISP domains */
+  "hotmail.com.ar", "live.com.ar", "yahoo.com.ar", "fibertel.com.ar", "speedy.com.ar", "arnet.com.ar",
+
+  /* Domains used in Mexico */
+  "hotmail.com", "gmail.com", "yahoo.com.mx", "live.com.mx", "yahoo.com", "hotmail.es", "live.com", "hotmail.com.mx", "prodigy.net.mx", "msn.com",
+
+  /* Domains used in Brazil */
+  "yahoo.com.br", "hotmail.com.br", "outlook.com.br", "uol.com.br", "bol.com.br", "terra.com.br", "ig.com.br", "itelefonica.com.br", "r7.com", "zipmail.com.br", "globo.com", "globomail.com", "oi.com.br"
+];
